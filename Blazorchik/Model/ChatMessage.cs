9995b4b4bfe934924e975793641dbef7d1de441c
Blur72 <adel.shaihutdinov123@gmail.com>
2025-03-01 12:36:31 +0300

    3

---
+++ Blazorchik/Model/ChatMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazorchik.Model
+{
+    public class ChatMessage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string MovieId { get; set; } // Идентификатор фильма
+
+        [Required]
+        public string UserId { get; set; } // Идентификатор пользователя (ключ)
+
+        [Required]
+        public string UserName { get; set; } // Имя пользователя
+
+        [Required]
+        public string Message { get; set; } // Текст сообщения
+
+        [Required]
+        public DateTime Timestamp { get; set; } // Время отправки сообщения
+    }
+}
---
+++ Blazorchik/Model/UserChat.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blazorchik.Model
+{
+    public class UserChat
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string SenderId { get; set; }
+        [Required]
+        public string SenderName { get; set; }
+        [Required]
+        public string RecipientId { get; set; }
+        [Required]
+        public string Message { get; set; }
+        public string ImageUrl { get; set; } // URL изображения
+        [Required]
+        public DateTime Timestamp { get; set; }
+    }
+}
